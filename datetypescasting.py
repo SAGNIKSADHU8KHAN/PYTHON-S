d22471fe0d3ce213d5c7933f8bb91c6ce1582794
SAGNIKSADHUKHAN <lakshmisadhukhan@95.com>
2025-09-02 18:16:39 +0530

    poligon

---
+++ datetypescasting.py
@@ -0,0 +1,17 @@
+name = "Penguin" 
+age = 8 
+is_student = True 
+weight = 32.5 
+
+print(name) 
+print("Date types of name",  type(name))  
+print(age) 
+print("Data types of age", type(age))
+print(is_student) 
+print("Data types of is_student is ", type(is_student)) 
+print(weight) 
+print("Data types of weight", type(weight)) 
+
+age = str(age) 
+
+print("Data types of age", type(age))
---
+++ poligone.py
@@ -0,0 +1,39 @@
+import turtle 
+
+turtle.Screen().bgcolor("green") 
+
+board = turtle.Turtle() 
+
+# Tringle 
+
+board.forward(100) 
+board.left(120) 
+
+board.forward(100) 
+board.left(120)
+
+
+board.forward(100) 
+
+# Square 
+
+board.forward(100) 
+board.left(90) 
+
+
+board.forward(100) 
+board.left(90) 
+
+board.forward(100) 
+board.left(90) 
+
+
+board.forward(100) 
+board.left(90) 
+
+turtle.done()
+
+
+
+
+
