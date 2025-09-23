b8e2d6415df4f99f2e0867481b44aaa927cf4c08
SAGNIKSADHUKHAN <lakshmisadhukhan@95.com>
2025-09-23 18:07:50 +0530

    dictanaries

---
+++ dictanaries.py
@@ -0,0 +1,24 @@
+my_dict = {} 
+
+my_dict = { 1 :"apple", 2 : "banana" } 
+
+my_dict = {"name": "mina", 1 : [2, 3, 4, 5, 7,]} 
+
+my_dict = {"name": "Raju", "age": 60} 
+
+print(my_dict["name"]) 
+print(my_dict.get("age")) 
+
+my_dict ["age"] = 12 
+
+print(my_dict) 
+
+my_dict.pop("age") 
+print(my_dict) 
+
+my_dict ["address"] = "Kolorah"
+print(my_dict) 
+
+my_dict.clear() 
+
+print(my_dict)
