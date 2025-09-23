fae897b5b0daabda9a1e41addf65cdf28672cf69
SAGNIKSADHUKHAN <lakshmisadhukhan@95.com>
2025-09-23 18:26:02 +0530

    tuples

---
+++ tuples.py
@@ -0,0 +1,19 @@
+my_tuple = {} 
+print(my_tuple) 
+
+my_tuple = (1, 2, 3, 4, 5,) 
+print(my_tuple) 
+
+my_tuple = (1, "hello", [1, 2, 3, "hi"]) 
+print(my_tuple) 
+
+my_tuple = (1, 2, 3, (4, 5, 6,), ["hi", "hello" 4])
+print(my_tuple) 
+
+my_tuple = ("w", "e", "r", "y", "s", "f", "g") 
+print(my_tuple[4]) 
+print(my_tuple[2]) 
+
+n_tuple = (1, 2, 3, (4, 5, 6,), ["hi", "hello" 4]) 
+
+print(n_tuple[3][1])
