275ec8fad630ccb0c0d5e1ac2d4f11e832d7884c
SAGNIKSADHUKHAN <lakshmisadhukhan@95.com>
2025-09-25 17:56:56 +0530

    patterns

---
+++ patters.py
@@ -0,0 +1,20 @@
+print("Basic star pattern") 
+
+for i in range(1, 6): 
+
+    for j in range(i): 
+
+        print("*", end=" ") 
+         
+    print("\n") 
+
+print("Inverted star pattern") 
+
+for i in range(5, 1, -1,): 
+
+    for j in range(i, 1, -1): 
+
+         print("*", end=" ") 
+
+    print("\n") 
+         
